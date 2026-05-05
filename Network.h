e385af57ef1ee4d6b3120a466e440e4cd765e7a9
matyaspopela <popela.matyas@student.gymzr.cz>
2026-05-05 18:38:15 +0200

    Prezentace & Onepager

---
+++ Network.h
@@ -0,0 +1,16 @@
+//
+// Created by matya on 5/5/2026.
+//
+
+#ifndef CNN_FROM_SCRATCH_NETWORK_H
+#define CNN_FROM_SCRATCH_NETWORK_H
+
+
+
+class Network {
+
+};
+
+
+
+#endif //CNN_FROM_SCRATCH_NETWORK_H
